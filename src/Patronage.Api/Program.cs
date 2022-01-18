3fb1d06342104c6e8c65e7e243cf85712d65e76c
Maurycy <faceaccont@gmail.com>
2022-01-18 18:07:35 +0100

    P2022-48 adds dbcontext

--- src/Patronage.Api/Program.cs
+++ src/Patronage.Api/Program.cs
@@ -1,8 +1,14 @@
+using Patronage.Models;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<TableContext>(opt =>
+    opt.UseInMemoryDatabase("TableList"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ src/Patronage.Models/DbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Patronage.Models;
+public class TableContext : DbContext
+{
+    public DbSet<Table> Tables { get; set; }
+
+    public TableContext(DbContextOptions<TableContext> options)
+            : base(options)
+    {
+
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        throw new NotImplementedException();
+    }
+}
