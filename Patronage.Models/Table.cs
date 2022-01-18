e61468dab4f7b04a3caeeeb6037856e914d0a1d9
Archanial <faceaccont@gmail.com>
2022-01-18 13:27:26 +0100

    table class + bumps dotnet to 6 and updates nuget packs

--- Patronage.Models/Table.cs
+++ Patronage.Models/Table.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Patronage.Models
-{
-    public class Class1
+{ 
+    public class Table
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "nvarchar(255)")]
+        [Required]
+        public string Name { get; set; }
+
+        [Column(TypeName = "nvarchar(255)")]
+        public string Description { get; set; }
+
     }
 }
