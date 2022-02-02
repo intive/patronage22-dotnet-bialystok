7c77028bef12d0abd9d4f11df8fdb4102e31234d
Dorian Margraf <dorian.margraf@gmail.com>
2022-02-02 21:27:41 +0100

    P2022-512 - Add Board Models

---
+++ src/Patronage.Contracts/ModelDtos/BoardDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patronage.Contracts.ModelDtos
+{
+    internal class BoardDto
+    {
+        public int Id { get; set; }
+        public string Alias { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int ProjectId { get; set; }
+        public int StatusId { get; set; }
+        public int? BoardId { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime ModifiedOn { get; set; }
+    }
+}
---
+++ src/Patronage.Models/Models/Board.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patronage.Models
+{
+    public class Board
+    {
+        public int Id { get; set; }
+
+        [MaxLength(256)]
+        public string Alias { get; set; }
+
+        [MaxLength(1024)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public int ProjectId { get; set; }
+
+        public bool IsActive { get; set; }
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime ModifiedOn { get; set; }
+
+    }
+}
