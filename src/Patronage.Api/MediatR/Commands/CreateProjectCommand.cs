6cb15aea919426fb3e0fa46144e0512ef90a3585
Dorian Margraf <dorian.margraf@gmail.com>
2022-02-05 21:34:31 +0100

    P2022-504 - Create Project Command Handler

---
+++ src/Patronage.Api/MediatR/Commands/CreateProjectCommand.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Patronage.Contracts.Interfaces;
+using Patronage.Contracts.ModelDtos;
+
+namespace Patronage.Api.MediatR.Commands
+{
+    public class CreateProjectCommand : IRequest<ProjectDto>
+    {
+        public ProjectDto Project { get; set; }
+    }
+
+    public class CreateProjectCommandHandler : IRequestHandler<CreateProjectCommand, ProjectDto>
+    {
+        private readonly IProjectService _projectService;
+
+        public CreateProjectCommandHandler(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
+        public Task<ProjectDto> Handle(CreateProjectCommand request, CancellationToken cancellationToken)
+        {
+            return _projectService.Create(request.Project);  
+        }
+    }
+}
