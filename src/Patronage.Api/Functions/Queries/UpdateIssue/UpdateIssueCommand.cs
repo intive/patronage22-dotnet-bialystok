f8fa6a837c80152136428d2e048677d3ef37c7e3
Kamil Świderski <swiderski.kamil.1998@gmail.com>
2022-02-09 16:05:14 +0100

    P2022-519 - init task

---
+++ src/Patronage.Api/Functions/Queries/UpdateIssue/UpdateIssueCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace Patronage.Api.Functions.Queries.UpdateIssue
+{
+    public class UpdateIssueCommand : IRequest
+    {
+        public int Id { get; set; }
+        public string Alias { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int ProjectId { get; set; }
+        public int? BoardId { get; set; }
+        public int StatusId { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime ModifiedOn { get; set; }
+    }
+}
---
+++ src/Patronage.Api/Functions/Queries/UpdateIssue/UpdateIssueCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+using Patronage.Contracts.Interfaces;
+using Patronage.Models;
+
+namespace Patronage.Api.Functions.Queries.UpdateIssue
+{
+    public class UpdateIssueCommandHandler : IRequestHandler<UpdateIssueCommand, Unit>
+    {
+        private readonly IIssueService _issueService;
+        private readonly IMapper _mapper;
+
+        public UpdateIssueCommandHandler(IIssueService issueService, IMapper mapper)
+        {
+            _issueService = issueService;
+            _mapper = mapper;
+        }
+
+        public Task<Unit> Handle(UpdateIssueCommand request, CancellationToken cancellationToken)
+        {
+
+
+            return Task.FromResult(Unit.Value);
+        }
+    }
+}
