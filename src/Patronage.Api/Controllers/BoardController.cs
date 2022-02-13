8d8dd86c4e9054327dd58230dfc9d591de2048b0
Mikołaj Czop <mikolajczop00@gmail.com>
2022-02-13 10:52:04 +0100

    P2022-510 Add Swagger annotations and fix returned types

--- src/Patronage.Api/Controllers/BoardController.cs
+++ src/Patronage.Api/Controllers/BoardController.cs
@@ -18,21 +18,28 @@ namespace Patronage.Api.Controllers
 
         [SwaggerOperation(Summary = "Create Board.")]
         [HttpPost("create")]
-        public ActionResult<IEnumerable<BoardDto>> CreateBoard([FromBody] BoardDto filter)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult CreateBoard([FromBody] BoardDto filter)
         {
-            return Ok(boardService.CreateBoard(filter));
+            var result = boardService.CreateBoard(filter);
+            return Ok();
         }
 
         [SwaggerOperation(Summary = "Returns all Boards or filter Boards by Alias, Name, Description.")]
         [HttpGet("list")]
-        public ActionResult<IEnumerable<BoardDto>> GetBoards([FromBody] BoardDto filter)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<IEnumerable<BoardDto>> GetBoards([FromQuery] FilterBoardDto? filter = null)
         {
-            return Ok(boardService.GetBoards(filter));
+            var boards = boardService.GetBoards(filter);
+            return Ok(boards);
         }
 
         [SwaggerOperation(Summary = "Return Board by Id.")]
         [HttpGet("{id:int}")]
-        public ActionResult<IEnumerable<BoardDto>> GetBoardById(int id)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<BoardDto> GetBoardById(int id)
         {
             var board = boardService.GetBoardById(id);
             if (board is null)
@@ -43,30 +50,40 @@ namespace Patronage.Api.Controllers
 
         [SwaggerOperation(Summary = "Full Board update. Expect complete Board's data.")]
         [HttpPut("update")]
-        public ActionResult<IEnumerable<BoardDto>> UpdateBoard([FromBody] BoardDto boardDto)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult UpdateBoard([FromBody] BoardDto boardDto)
         {
             var result = boardService.UpdateBoard(boardDto);
-            return Ok(result);
+            return Ok();
         }
 
-        [SwaggerOperation(Summary = "Light Board Update")]
-        [HttpPut("updateLight")]
-        public ActionResult<IEnumerable<BoardDto>> UpdateBoardLight([FromBody] BoardDto boardDto)
+        [SwaggerOperation(Summary = "Updates Board - only selected properties")]
+        [HttpPatch("updateLight")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult UpdateBoardLight([FromBody] PartialBoardDto boardDto)
         {
-            var result = boardService.UpdateBoard(boardDto);
-            return Ok(result);
+            var result = boardService.UpdateBoardLight(boardDto);
+            if (!result)
+            {
+                return NotFound();
+            }
+            return Ok();
         }
 
         [SwaggerOperation(Summary = "Soft delete Board by Id")]
         [HttpPut("delete/{id:int}")]
-        public ActionResult<IEnumerable<BoardDto>> DeleteBoard(int id)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult DeleteBoard(int id)
         {
             var result = boardService.DeleteBoard(id);
             if (!result)
             {
                 return NotFound();
             }
-            return Ok(result);
+            return Ok();
         }
     }
 }
