2962889850ce91c07cfbaa7309d4139a4168d14a
Krzysztof <Krzysztofdubilo@gmail.com>
2022-02-15 11:25:48 +0100

    Revert "P2022-513-514-778- Clean project"
    This reverts commit be5eaa6da4b84d504b91ef73d354d092fb6539e5.

---
+++ src/Patronage.Api/MediatR/Queries/SearchProjectQuery.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Patronage.Contracts.Interfaces;
+using Patronage.Contracts.ModelDtos;
+
+namespace Patronage.Api.MediatR.Queries
+{
+    public class SearchProjectQuery : IRequest<ProjectDto>
+    {
+        public int Id { get; set; }
+        public SearchProjectQuery(int id) 
+        { Id = id; }
+        public ProjectDto Project { get; set; }
+    }
+
+    public class SearchProjectQueryHandler : IRequestHandler<SearchProjectQuery, ProjectDto>
+    {
+        private readonly IProjectService _projectService;
+
+        public SearchProjectQueryHandler(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
+        public Task<ProjectDto> Handle(SearchProjectQuery request, CancellationToken cancellationToken)
+        {
+            _projectService.Update(request.Id, request.Project);
+            return Task.FromResult(request.Project);
+        }
+    }
+}
