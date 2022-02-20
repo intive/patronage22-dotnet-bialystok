2a87a0a1e8b8d8065c502592d8f38969aca1ff96
Krzysztof <Krzysztofdubilo@gmail.com>
2022-02-20 18:54:11 +0100

    P2022-1063 - Add test data for Projects, Boards and Issues, using existing Seeder class.

--- src/Patronage.DataAccess/DataSeeder.cs
+++ src/Patronage.DataAccess/DataSeeder.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Patronage.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Patronage.DataAccess
 {
@@ -32,6 +27,64 @@ namespace Patronage.DataAccess
                     _logger.LogInformation("Applying Migration");
                     _dbContext.Database.Migrate();
                 }
+
+                /*----------------------------------------------------*/
+                /*--------------- Seeding test projects --------------*/
+                /*----------------------------------------------------*/
+                if (!_dbContext.Projects.Any())
+                {
+                    _dbContext.Projects.AddRange(produceTestProjects());
+                    _dbContext.SaveChanges();
+                }
+
+
+                /*----------------------------------------------------*/
+                /*--------------- Seeding test boards ----------------*/
+                /*----------------------------------------------------*/
+                if (!_dbContext.Boards.Any())
+                {
+                    _dbContext.Boards.AddRange(produceTestBoards());
+                    _dbContext.SaveChanges();
+                }
+
+
+                /*----------------------------------------------------*/
+                /*--------------- Seeding test issues ----------------*/
+                /*----------------------------------------------------*/
+                if (!_dbContext.Issues.Any())
+                {
+                    _dbContext.Issues.AddRange(produceTestIssues());
+                    _dbContext.SaveChanges();
+                }
+
+                /*----------------------------------------------------*/
+                /*-------------- Seeding test statuses ---------------*/
+                /*----------------------------------------------------*/
+                //if (!_dbContext.Statuses.Any())
+                //{
+                //    _dbContext.Boards.AddRange(produceTestStatuses());
+                //    _dbContext.SaveChanges();
+                //}
+
+
+                /*----------------------------------------------------*/
+                /*----------- Seeding test BoardStatuses -------------*/
+                /*----------------------------------------------------*/
+                //if (!_dbContext.BoardStatuses.Any())
+                //{
+                //    _dbContext.Issues.AddRange(produceTestBoardStatuses());
+                //    _dbContext.SaveChanges();
+                //}
+
+
+                /*----------------------------------------------------*/
+                /*------------- Seeding test Admin User --------------*/
+                /*----------------------------------------------------*/
+                //if (!_dbContext.Users.Any())
+                //{
+                //    _dbContext.Users.AddRange(produceTestUsers());
+                //    _dbContext.SaveChanges();
+                //}
             }
             else
             {
@@ -39,5 +92,158 @@ namespace Patronage.DataAccess
                 throw new Exception("Unable to connect to database");
             }
         }
+
+
+
+        private IEnumerable<Project> produceTestProjects()
+        {
+            var projects = new List<Project>()
+            {
+                new Project()
+                {
+                    Name = "First project",
+                    Alias = "1st",
+                    Description = "This is a description of first test project",
+                    IsActive = true
+                },
+
+                new Project()
+                {
+                    Name = "Second test project",
+                    Alias = "2nd",
+                    Description = "This is a description of 2nd test project",
+                    IsActive = false
+                },
+
+                new Project()
+                {
+                    Name = "Third test project",
+                    Alias = "3rd",
+                    Description = null,
+                    IsActive = false
+                },
+            };
+
+            return projects;
+        }
+
+
+        private IEnumerable<Board> produceTestBoards()
+        {
+            var boards = new List<Board>()
+            {
+                new Board()
+                {
+                    Name = "First test Board",
+                    Alias = "1st board",
+                    Description = "This is a description of first test board",
+                    ProjectId = 1,
+                    IsActive = true
+                },
+
+                new Board()
+                {
+                    Name = "Second test Board",
+                    Alias = "2nd board",
+                    Description = "This is a description of second test board",
+                    ProjectId = 1,
+                    IsActive = false
+                },
+            };
+
+            return boards;
+        }
+
+
+
+        private IEnumerable<Issue> produceTestIssues()
+        {
+            var issues = new List<Issue>()
+            {
+                new Issue
+                {
+                    Name = "First test Issue",
+                    Alias = "1st issue",
+                    Description = "This is a description of first test issue. This Issue is connected to a Board",
+                    ProjectId = 1,
+                    StatusId = 1,
+                    BoardId = 1,
+                    IsActive = true
+                },
+
+                new Issue
+                {
+                    Name = "Second test Issue",
+                    Alias = "2nd issue",
+                    Description = "This is a description of second test issue. This Issue isn't connected to a Board",
+                    ProjectId = 1,
+                    StatusId = 1,
+                    BoardId = null,
+                    IsActive = true
+                },
+            };
+
+            return issues;
+        }
+
+/*
+        private IEnumerable<Status> produceTestStatuses()
+        {
+            var statuses = new List<Status>()
+            {
+                new Status
+                {
+
+                },
+
+                new Status
+                {
+
+                }
+            };
+
+            return statuses;
+        }
+
+
+
+        private IEnumerable<BoardStatus> produceTestBoardStatuses()
+        {
+            var boardStatuses = new List<BoardStatus>()
+            {
+                new BoardStatus
+                {
+
+                },
+
+                new BoardStatus
+                {
+
+                }
+            };
+
+            return boardStatuses;
+        }
+
+
+        private IEnumerable<User> produceTestUsers()
+        {
+            var users = new List<User>()
+            {
+                new User
+                {
+
+                },
+
+                new User
+                {
+
+                }
+            };
+
+            return users;
+        }
+*/
+
     }
 }
