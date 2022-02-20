be2740d754cce02be58ef916acac1e3535a9d742
Kamil Świderski <swiderski.kamil.1998@gmail.com>
2022-02-20 11:44:35 +0100

    Delete register validator in program.cs, change httpput to httppatch in light update, create validator for CreateIsueCommand

---
+++ src/Patronage.Api/Validators/CreateIssueValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Patronage.Api.MediatR.Issues.Commands.CreateIssue;
+
+namespace Patronage.Api.Validators
+{
+    public class CreateIssueValidator : AbstractValidator<CreateIssueCommand>
+    {
+        public CreateIssueValidator()
+        {
+            RuleFor(r => r.ProjectId).GreaterThanOrEqualTo(1);
+        }
+    }
+}
