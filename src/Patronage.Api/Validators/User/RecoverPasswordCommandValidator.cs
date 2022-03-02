c017a691c5fb2342a9530e6923b5e6cd105bcbd7
Mikołaj Czop <mikolajczop00@gmail.com>
2022-03-02 16:46:00 +0100

    P2022-1418 Add validators

---
+++ src/Patronage.Api/Validators/User/RecoverPasswordCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Patronage.Api.MediatR.User.Commands.Password;
+
+namespace Patronage.Api.Validators.User
+{
+    public class RecoverPasswordCommandValidator : AbstractValidator<RecoverPasswordCommand>
+    {
+        public RecoverPasswordCommandValidator(NewUserPasswordValidator validator)
+        {
+            RuleFor(x => x.NewUserPassword).SetValidator(validator);
+        }
+    }
+}
