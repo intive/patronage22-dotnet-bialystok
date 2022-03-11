ba6f70c6094529861d72577385859518ed9efc35
Mikołaj Czop <mikolajczop00@gmail.com>
2022-03-11 17:11:49 +0100

    P2022-1588 Change comments to XML

--- src/Patronage.Api/Controllers/BoardController.cs
+++ src/Patronage.Api/Controllers/BoardController.cs
@@ -22,10 +22,15 @@ namespace Patronage.Api.Controllers
             this.mediator = mediator;
         }
 
-        [SwaggerOperation(Summary = "Create Board.")]
+        /// <summary>
+        /// Create new Board based on object passed in request's body.
+        /// </summary>
+        /// <param name="boardDto">JSON object with properties defining a board to create</param>
+        /// <response code="201">Board was created successfully.</response>
+        /// <response code="400">Board could not be created.</response>
+        /// <returns>Created board or null if board could not be created.</returns>
         [HttpPost("create")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult> CreateBoard([FromBody] CreateBoardCommand boardDto)
+        public async Task<ActionResult<BoardDto>> CreateBoard([FromBody] CreateBoardCommand boardDto)
         {
             var result = await mediator.Send(boardDto);
 
@@ -33,22 +38,27 @@ namespace Patronage.Api.Controllers
             {
                 return BadRequest(new BaseResponse<BoardDto>
                 {
-                    ResponseCode = StatusCodes.Status400BadRequest,
+                    ResponseCode = StatusCodes.Status500InternalServerError,
+                    Message = "Board could not be created."
                 });
             }
 
-            return Ok(new BaseResponse<BoardDto>
+            return CreatedAtAction(nameof(CreateBoard), new BaseResponse<BoardDto>
             {
-                Message = "Board was created successfully",
-                ResponseCode = StatusCodes.Status200OK,
+                ResponseCode = StatusCodes.Status201Created,
+                Message = "Board was created successfully.",
                 Data = result
             });
         }
 
-        [SwaggerOperation(Summary = "Returns all Boards or filter Boards by Alias, Name, Description.")]
+        /// <summary>
+        /// Returns all Boards or filtered Boards by Alias, Name or Description.
+        /// </summary>
+        /// <param name="filter">Object created from query containing Alias, Name and Descripion.</param>
+        /// <response code="200">Boards was fetched successfully.</response>
+        /// <response code="404">There's no boards.</response>
+        /// <returns>All Board or filtered Boards if filter is presents.</returns>
         [HttpGet("list")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<BaseResponse<IEnumerable<BoardDto>>>> GetBoards([FromQuery] FilterBoardDto filter)
         {
             var query = new GetBoardsQuery(filter);
@@ -60,21 +70,26 @@ namespace Patronage.Api.Controllers
                 return NotFound(new BaseResponse<IEnumerable<BoardDto>>
                 {
                     ResponseCode = StatusCodes.Status404NotFound,
-                    Message = $"There's no boards"
+                    Message = "There's no boards."
                 });
             }
 
             return Ok(new BaseResponse<IEnumerable<BoardDto>>
             {
                 ResponseCode = StatusCodes.Status200OK,
+                Message = "Boards was fetched successfully.",
                 Data = result
             });
         }
 
-        [SwaggerOperation(Summary = "Return Board by Id.")]
+        /// <summary>
+        /// Return Board by Id.
+        /// </summary>
+        /// <response code="200">Board was fetched successfully.</response>
+        /// <response code="404">There's no board with requested Id.</response>
+        /// <returns>Board with specified id.</returns>
+        /// <param name="id">Board's id.</param>
         [HttpGet("{id:int}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<BoardDto>> GetBoardById(int id)
         {
             var query = new GetBoardByIdQuery(id);
@@ -93,15 +108,20 @@ namespace Patronage.Api.Controllers
             return Ok(new BaseResponse<BoardDto>
             {
                 ResponseCode = StatusCodes.Status200OK,
+                Message = "Board was fetched successfully.",
                 Data = result
             });
         }
-
-        [SwaggerOperation(Summary = "Full Board update. Expect complete Board's data.")]
+        
+        /// <summary>
+        /// Full Board update. Expect complete Board's data.
+        /// </summary>
+        /// <param name="boardDto"></param>
+        /// <response code="200">Board was updated successfully.</response>
+        /// <response code="404">There's no board with requested Id.</response>
+        /// <returns>True if board was updated successfully.</returns>
         [HttpPut("update")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult> UpdateBoard([FromBody] UpdateBoardCommand boardDto)
+        public async Task<ActionResult<bool>> UpdateBoard([FromBody] UpdateBoardCommand boardDto)
         {
             var result = await mediator.Send(boardDto);
 
@@ -110,23 +130,29 @@ namespace Patronage.Api.Controllers
                 return NotFound(new BaseResponse<bool>
                 {
                     ResponseCode = StatusCodes.Status404NotFound,
-                    Message = $"There's no board with Id: {boardDto.Data.Id}"
+                    Message = $"There's no board with Id: {boardDto.Data.Id}",
+                    Data = false
                 });
             }
 
             return Ok(new BaseResponse<bool>
             {
-                Message = "Board was updated successfully",
+                Message = "Board was updated successfully.",
                 ResponseCode = StatusCodes.Status200OK,
                 Data = result
             });
         }
 
+        /// <summary>
+        /// Updates Board - only selected properties such as Alias, Name and Description.
+        /// </summary>
+        /// <param name="boardDto"></param>
+        /// <response code="200">Board was updated successfully.</response>
+        /// <response code="404">There's no board with requested Id.</response>
+        /// <returns>True if board was updated successfully.</returns>
         [SwaggerOperation(Summary = "Updates Board - only selected properties")]
         [HttpPatch("updateLight")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult> UpdateBoardLight([FromBody] UpdateBoardLightCommand boardDto)
+        public async Task<ActionResult<bool>> UpdateBoardLight([FromBody] UpdateBoardLightCommand boardDto)
         {
             var result = await mediator.Send(boardDto);
 
@@ -135,7 +161,8 @@ namespace Patronage.Api.Controllers
                 return NotFound(new BaseResponse<bool>
                 {
                     ResponseCode = StatusCodes.Status404NotFound,
-                    Message = $"There's no board with Id: {boardDto.Data.Id}"
+                    Message = $"There's no board with Id: {boardDto.Data.Id}",
+                    Data = false
                 });
             }
 
@@ -147,11 +174,15 @@ namespace Patronage.Api.Controllers
             });
         }
 
-        [SwaggerOperation(Summary = "Soft delete Board by Id")]
+        /// <summary>
+        /// Soft delete Board with specified Id
+        /// </summary>
+        /// <param name="id">Board's id</param>
+        /// <response code="200">Board was deleted successfully.</response>
+        /// <response code="404">There's no board with requested Id.</response>
+        /// <returns>True if Board was deleted successfully.</returns>
         [HttpDelete("delete/{id:int}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult> DeleteBoard(int id)
+        public async Task<ActionResult<bool>> DeleteBoard(int id)
         {
             var result = await mediator.Send(new DeleteBoardCommand { Id = id });
 
@@ -160,7 +191,8 @@ namespace Patronage.Api.Controllers
                 return NotFound(new BaseResponse<bool>
                 {
                     ResponseCode = StatusCodes.Status404NotFound,
-                    Message = $"There's no board with Id: {id}"
+                    Message = $"There's no board with Id: {id}",
+                    Data = false
                 });
             }
 
