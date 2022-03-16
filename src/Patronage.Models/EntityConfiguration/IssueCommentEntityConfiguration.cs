2b5493bd2ab12be7fda769273bda814304f317ce
Kamil Świderski <swiderski.kamil.1998@gmail.com>
2022-03-16 17:35:38 +0100

    fic entity configuration

--- src/Patronage.Models/EntityConfiguration/IssueCommentEntityConfiguration.cs
+++ src/Patronage.Models/EntityConfiguration/IssueCommentEntityConfiguration.cs
@@ -12,20 +12,28 @@ namespace Patronage.Models.EntityConfiguration
     {
         public void Configure(EntityTypeBuilder<Comment> builder)
         {
+
             builder
-                .Property(r => r.Content)
-                .HasMaxLength(500);
+                 .Property(r => r.IssueId)
+                 .IsRequired();
+
+            builder
+                 .Property(r => r.Content)
+                 .HasMaxLength(500);
+
+            builder
+                 .Property(r => r.CreatedOn)
+                 .IsRequired();
+
+            builder
+                .HasOne(p => p.Issue)
+                .WithMany(b => b.Comment)
+                .HasForeignKey(p => p.IssueId);
 
             builder
                 .HasOne(p => p.User)
                 .WithMany(b => b.Comment)
                 .HasForeignKey(p => p.ApplicationUserId);
-
-            builder
-                .HasOne(p => p.Issue)
-                .WithMany(b => b.Comment)
-                .HasForeignKey(p => p.ApplicationUserId);
-
         }
     }
 }
