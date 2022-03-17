6f776e9e9947f13bc021eabfec3cba04b24b3ca9
Kamil Świderski <swiderski.kamil.1998@gmail.com>
2022-03-17 17:33:27 +0100

    add  = null!

--- src/Patronage.Contracts/ModelDtos/User/SignInDto.cs
+++ src/Patronage.Contracts/ModelDtos/User/SignInDto.cs
@@ -2,7 +2,7 @@
 {
     public class SignInDto
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
     }
 }
