0876dd7c7ef851c0afd07e2cce2b32feb58b4ee1
Kamil Świderski <swiderski.kamil.1998@gmail.com>
2022-03-17 17:19:48 +0100

    Add filter issue by board id

--- src/Patronage.Contracts/ModelDtos/Issues/FilterIssueDto.cs
+++ src/Patronage.Contracts/ModelDtos/Issues/FilterIssueDto.cs
@@ -2,6 +2,7 @@
 {
     public class FilterIssueDto
     {
+        public int? BoardId { get; set; }
         public string? SearchPhrase { get; set; }
         public int PageNumber { get; set; }
         public int PageSize { get; set; }
