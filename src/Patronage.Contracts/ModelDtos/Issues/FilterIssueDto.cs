67f06a5fbb6df3a5ec48eb73440e5c15bafef34b
Kamil Świderski <swiderski.kamil.1998@gmail.com>
2022-03-18 12:42:16 +0100

    - set default values to PageSize and PageNumber - create static class for allow page size - cascade delete disable in fk columns - migrations with disable cascade delete
    (cherry picked from commit 28403d2f173a22aa026cc520b25f34d9fd379a67)

--- src/Patronage.Contracts/ModelDtos/Issues/FilterIssueDto.cs
+++ src/Patronage.Contracts/ModelDtos/Issues/FilterIssueDto.cs
@@ -4,7 +4,7 @@
     {
         public int? BoardId { get; set; }
         public string? SearchPhrase { get; set; }
-        public int PageNumber { get; set; }
-        public int PageSize { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 5;
     }
 }
