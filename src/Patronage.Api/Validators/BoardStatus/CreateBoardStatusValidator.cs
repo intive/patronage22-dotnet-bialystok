20f2bc8903d73e6cc2ca866b4a3d5638db9b2c1e
Kamil Świderski <swiderski.kamil.1998@gmail.com>
2022-03-25 16:54:22 +0100

    add suggestion from pr

--- src/Patronage.Api/Validators/BoardStatus/CreateBoardStatusValidator.cs
+++ src/Patronage.Api/Validators/BoardStatus/CreateBoardStatusValidator.cs
@@ -15,12 +15,62 @@ namespace Patronage.Api.Validators.BoardStatus
             RuleFor(s => s.Dto.BoardId)
                 .NotNull()
                 .NotEmpty()
-                .GreaterThanOrEqualTo(1);
+                .GreaterThanOrEqualTo(1)
+                .Must(ExistsBoardId).WithMessage("BoardId does not exist.");
 
             RuleFor(s => s.Dto.StatusId)
                 .NotNull()
                 .NotEmpty()
-                .GreaterThanOrEqualTo(1);
+                .GreaterThanOrEqualTo(1)
+                .Must(ExistsStatusId).WithMessage("StatusId does not exist.");
+
+
+            RuleFor(x => new { x.Dto.BoardId, x.Dto.StatusId })
+                .Must(m => ExistsBoardStatus(m.BoardId, m.StatusId))
+                .WithMessage("BoardStatus with specified boardId and statusId already exists.");
+        }
+
+        public bool ExistsBoardStatus(int boardId, int statusId)
+        {
+            var boardStatus = _context
+                .BoardsStatus
+                .Where(b => b.BoardId.Equals(boardId))
+                .Where(b => b.StatusId.Equals(statusId));
+            if (boardStatus.Any())
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private bool ExistsBoardId(int boardId)
+        {
+            var _boardId = _context
+                .Boards
+                .Where(b => b.Id.Equals(boardId));
+            if (_boardId.Any())
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+        private bool ExistsStatusId(int statusId)
+        {
+            var _statusId = _context
+                .Statuses
+                .Where(b => b.Id.Equals(statusId));
+            if (_statusId.Any())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
