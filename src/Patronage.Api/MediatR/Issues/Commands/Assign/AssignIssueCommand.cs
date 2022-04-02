474c8be9c3a62787b4d287006e0ee53d797df98b
Kamil Świderski <swiderski.kamil.1998@gmail.com>
2022-04-02 10:34:49 +0200

    assign userId can be null

--- src/Patronage.Api/MediatR/Issues/Commands/Assign/AssignIssueCommand.cs
+++ src/Patronage.Api/MediatR/Issues/Commands/Assign/AssignIssueCommand.cs
@@ -5,9 +5,9 @@ namespace Patronage.Api.MediatR.Issues.Commands
     public class AssignIssueCommand : IRequest<bool>
     {
         public int IssueId { get; set; }
-        public string UserId { get; set; }
+        public string? UserId { get; set; }
 
-        public AssignIssueCommand(int id, string userId)
+        public AssignIssueCommand(int id, string? userId)
         {
             IssueId = id;
             UserId = userId;
